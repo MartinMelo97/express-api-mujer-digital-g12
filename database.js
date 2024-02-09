4f39820de14db493cd801ccc7c76d4f521855742
Martin Melo <martin.melo.dev.97@gmail.com>
2024-02-08 19:01:36 -0600

    Tienda de abarrotes GET endpoints

---
+++ database.js
@@ -0,0 +1,33 @@
+const products = [
+    {
+        id: 1,
+        name: 'Mayonesa McCormick 500',
+        price: 60,
+        cantidadDisponible: 15,
+        categoria: 'Aderezos',
+        marca: 'McCormick',
+        descripcion: 'Ay perdón, Hellmanns Hellmanns'
+    },
+    {
+        id: 2,
+        name: 'Sabritas Adobadas',
+        price: 22,
+        cantidadDisponible: 30,
+        categoria: 'Botana',
+        marca: 'Sabritas',
+        descripcion: 'A que no puedes comer sólo una'
+    },
+    {
+        id: 3,
+        name: 'Maruchan Camarones',
+        price: 17,
+        cantidadDisponible: 50,
+        categoria: 'Sopa Instantánea',
+        marca: 'Maruchan',
+        descripcion: 'Rica, adictiva y tapa intestinos'
+    }
+]
+
+module.exports = {
+    products
+}
--- index.js
+++ index.js
@@ -1,23 +1,57 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const { products } = require('./database');
+
+dotenv.config();
 
 const app = express();
 
-// www.google.com?hola=mundo&mundo=hola
-// www.google.com/hola/mundo
+// Products Endpoints
+
+// 1. GET /products -> Todos los productos
+// 2. GET /products/1 -> Entrega UN sólo producto
+// 3. POST /products -> Crear un nuevo producto
+// 4. PUT /products/1 -> Editar un producto
+// 5. PATCH /products/1 -> Editar un producto (parcial)
+// 6. DELETE /products/1 -> Elimina un producto
+
+app.get('/products', function(req, res) {
+    return res.status(200).json(products);
+});
+
+app.get('/products/:id', function(req, res) {
+    // Siempre que leemos valores desde req.params
+    // el valor que leemos es de tipo STRING.
+    const { id } = req.params;
+
+    const product = products.find(function(product) {
+        return product.id === Number(id)
+    });
+
+    if (!product) {
+        return res.status(404).json({
+            message: 'No se encontró el producto con el ID: ' + id
+        })
+    }
+
+    return res.status(200).json(product);
+});
 
 app.get('/', function(req, res) {
-    res.status(200).json({
-        message: 'Hola Mundo Mujer Digital!'
+    return res.status(200).json({
+        message: 'Hola Mundo Mujer Digital 2!'
     });
 });
 
 app.get('/:name', function(req, res) {
-    const name = req.params.name;
-    res.status(200).json({
+    const { name } = req.params;
+    return res.status(200).json({
         message: `Hola! ${name}, bienvenido a mi API`
     });
 });
 
-app.listen(8080, function() {
-    console.log('La API está corriendo en el puerto 8080');
+const port = process.env.PORT;
+
+app.listen(port, function() {
+    console.log('La API está corriendo en el puerto ' + port);
 });
